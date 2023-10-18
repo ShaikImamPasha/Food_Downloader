fdbacb51226a81471e033f498b2b78482d6d1884
unknown <shaikimam916@gmail.com>
2023-10-18 20:20:14 +0530

    processing on bottem navbar

--- src/Componentes/BottemNavBar.js
+++ src/Componentes/BottemNavBar.js
@@ -1,7 +1,7 @@
 const BottemNavBar=()=>{
     return(
         <>
-        <div className="flex justify-around fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
+        <div className="flex justify-around fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200">
             <div>
             <span class="material-symbols-outlined">
 home
