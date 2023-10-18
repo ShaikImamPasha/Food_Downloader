09968925e1ca6766176e6a85d14103943050b50b
unknown <shaikimam916@gmail.com>
2023-10-18 20:26:24 +0530

    neww

--- src/Componentes/BottemNavBar.js
+++ src/Componentes/BottemNavBar.js
@@ -1,11 +1,13 @@
 const BottemNavBar=()=>{
     return(
         <>
-        <div className="flex justify-around fixed bottom-0 left-0 z-50 w-full h-10 bg-white border-t border-gray-200">
+        <div className="flex justify-around fixed bottom-0 left-0 z-50 w-full h-12 bg-white border-t border-gray-200">
             <div>
-            <span class="material-symbols-outlined">
+            <h1>
+            <span class="material-symbols-outlined w-48 h-48">
 home
 </span>
+            </h1>
             </div>
             <div>
             <span class="material-symbols-outlined">
