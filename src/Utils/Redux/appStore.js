d8532631789d23f9ecab99d3ccf005e6741b9d40
ShaikImamPasha <shaikimam916@gmail.com>
2023-12-26 04:41:49 -0800

    updating code

--- src/Utils/Redux/appStore.js
+++ src/Utils/Redux/appStore.js
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cardReducer from "./cardSlice";
 import locationes from "./locationes";
-import Commentes from "./Commentes";
 const appStore=configureStore({
     reducer:{
         cart: cardReducer,
